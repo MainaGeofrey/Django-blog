9c970add03a645473ddc72ee794f3eed707e0943
jeff <jeff@localhost.localdomain>
2022-05-31 11:38:09 +0300

    changd from sqlite3 to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'jeff',
+        'PASSWORD': 'j1024p',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
